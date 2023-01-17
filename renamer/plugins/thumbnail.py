46446b88dae1f7649cd9c0a1f25e6627193f41a8
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-18 02:08:47 +0530

    Update thumbnail.py

--- renamer/plugins/thumbnail.py
+++ renamer/plugins/thumbnail.py
@@ -4,7 +4,7 @@ logger = logging.getLogger(__name__)
 import os
 from ..config import Config
 from ..tools.text import TEXT
-from ..database.database import *
+#from ..database.database import *
 from pyrogram import Client as RenamerNs, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
